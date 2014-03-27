d4ec41d34cdda105127c1c5147aab58807b9299d
Graham Hay <grahamrhay@gmail.com>
2014-03-27 22:34:34 +0000

    Support for categories

--- src/opml_parser.erl
+++ src/opml_parser.erl
@@ -5,26 +5,34 @@
 -include("seymour.hrl").
 
 parse(Opml) ->
-    {ok, State, _} = erlsom:parse_sax(Opml, [], fun handle_event/2),
-    State.
+    {ok, {_, _, Feeds}, _} = erlsom:parse_sax(Opml, {0, "", []}, fun handle_event/2),
+    Feeds.
 
-handle_event({startElement, _NS, "outline", _, Attrs}, State) ->
+handle_event({startElement, _, "outline", _, Attrs}, {Depth, Category, Feeds}) ->
+    NewDepth = Depth + 1,
     case is_feed_element(Attrs) of
         true ->
             Feed = build_record(Attrs),
-            update_list(State, Feed);
+            {NewDepth, Category, update_list(Feeds, Feed, Category)};
         false ->
-            State
+            {NewDepth, get_attribute_value(Attrs, "text"), Feeds}
     end;
 
-handle_event(Event, State) ->
-    io:format("Event: ~p, State: ~p~n", [Event, State]),
+handle_event({endElement, _, "outline", _}, {Depth, Category, Feeds}) ->
+    NewDepth = Depth - 1,
+    NewCategory = case NewDepth of
+        0 -> "";
+        _ -> Category
+    end,
+    {NewDepth, NewCategory, Feeds};
+
+handle_event(_Event, State) ->
     State.
 
 is_feed_element(Attrs) ->
     Predicate = fun(A) ->
-            {attribute, Name, _, _, _} = A,
-            Name =:= "xmlUrl"
+        {attribute, Name, _, _, _} = A,
+        Name =:= "xmlUrl"
     end,
     lists:any(Predicate, Attrs).
 
@@ -42,10 +50,18 @@ get_attribute_value(Attrs, AttrName) ->
     {attribute, _, _, _, Value} = lists:nth(1, lists:filter(fun(A) -> {attribute, Name, _, _, _} = A, Name =:= AttrName end, Attrs)),
     Value.
 
-update_list(List, Feed) ->
-    case length(lists:filter(fun(F) -> F#feed.xmlUrl =:= Feed#feed.xmlUrl end, List)) of
+update_list(List, Feed, Category) ->
+    {MatchingFeeds, OtherFeeds} = lists:partition(fun(F) -> F#feed.xmlUrl =:= Feed#feed.xmlUrl end, List),
+    case length(MatchingFeeds) of
         0 ->
-            [Feed|List];
+            [Feed#feed{categories = update_category_list(Category, Feed#feed.categories)}|List];
         _ ->
-            List
+            [ExistingFeed] = MatchingFeeds,
+            [ExistingFeed#feed{categories = update_category_list(Category, ExistingFeed#feed.categories)}|OtherFeeds]
+    end.
+
+update_category_list(NewCategory, Categories) ->
+    case NewCategory of
+        "" -> Categories;
+        _ -> lists:reverse([NewCategory|Categories])
     end.
--- test/opml_parser_tests.erl
+++ test/opml_parser_tests.erl
@@ -17,19 +17,22 @@ test_feed1(FeedList) ->
     Feed1 = lists:nth(1, lists:filter(fun(F) -> F#feed.title =:= "feed1" end, FeedList)),
     [
         ?_assertEqual("http://feed1.com/home", Feed1#feed.htmlUrl),
-        ?_assertEqual("http://feed1.com/rss", Feed1#feed.xmlUrl)
+        ?_assertEqual("http://feed1.com/rss", Feed1#feed.xmlUrl),
+        ?_assertEqual(["category1", "category2"], Feed1#feed.categories)
     ].
 
 test_feed2(FeedList) ->
     Feed2 = lists:nth(1, lists:filter(fun(F) -> F#feed.title =:= "feed2" end, FeedList)),
     [
         ?_assertEqual("http://feed2.com/home", Feed2#feed.htmlUrl),
-        ?_assertEqual("http://feed2.com/rss", Feed2#feed.xmlUrl)
+        ?_assertEqual("http://feed2.com/rss", Feed2#feed.xmlUrl),
+        ?_assertEqual(["category1"], Feed2#feed.categories)
     ].
 
 test_feed3(FeedList) ->
     Feed3 = lists:nth(1, lists:filter(fun(F) -> F#feed.title =:= "feed3" end, FeedList)),
     [
         ?_assertEqual("http://feed3.com/home", Feed3#feed.htmlUrl),
-        ?_assertEqual("http://feed3.com/rss", Feed3#feed.xmlUrl)
+        ?_assertEqual("http://feed3.com/rss", Feed3#feed.xmlUrl),
+        ?_assertEqual([], Feed3#feed.categories)
     ].
